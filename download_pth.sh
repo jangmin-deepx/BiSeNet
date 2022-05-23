e751d18b16836c8efba57ddd6c338cc9a25d6a65
Janmgin Son <jangmin@deepx-local-gpu.deepx.wifi>
2022-05-23 15:36:30 +0900

    update

--- download_pth.sh
+++ download_pth.sh
@@ -1,2 +1,4 @@
-wget https://github.com/CoinCheung/BiSeNet/releases/download/0.0.0/model_final_v1_city_new.pth
-wget https://github.com/CoinCheung/BiSeNet/releases/download/0.0.0/model_final_v2_city.pth
+rm -rf pretrained_weight
+mkdir pretrained_weight
+wget -P pretrained_weight https://github.com/CoinCheung/BiSeNet/releases/download/0.0.0/model_final_v1_city_new.pth 
+wget -P pretrained_weight https://github.com/CoinCheung/BiSeNet/releases/download/0.0.0/model_final_v2_city.pth 
